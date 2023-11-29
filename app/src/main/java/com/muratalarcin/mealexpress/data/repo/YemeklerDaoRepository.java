6d00a675ddf95c4b8182da577440b983ad0d08f6
Murat Alarcin <murattrbl@icloud.com>
2023-11-30 00:09:53 +0300

    design vol 1 complated

--- app/src/main/java/com/muratalarcin/mealexpress/data/repo/YemeklerDaoRepository.java
+++ app/src/main/java/com/muratalarcin/mealexpress/data/repo/YemeklerDaoRepository.java
@@ -13,9 +13,13 @@ public class YemeklerDaoRepository {
         ArrayList<Yemekler> liste = new ArrayList<>();
         Yemekler y1 = new Yemekler(1, "Ayran", "caney.jpg", 50, 1, 55, "murat");
         Yemekler y2 = new Yemekler(2, "sss", "sevenler.jpg", 50, 2, 5, "ss");
+        Yemekler y3 = new Yemekler(2, "sss", "sevenler.jpg", 50, 2, 5, "ss");
+        Yemekler y4 = new Yemekler(2, "sss", "sevenler.jpg", 50, 2, 5, "ss");
 
         liste.add(y1);
         liste.add(y2);
+        liste.add(y3);
+        liste.add(y4);
         yemeklerListesi.setValue(liste);
     }
 }
